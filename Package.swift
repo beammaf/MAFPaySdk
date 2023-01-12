74169ece7b640fa6338ea9b6bff3751c6aa44ad7
basheerhader <51642234+basheerhader@users.noreply.github.com>
2023-01-12 17:26:45 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "MAFPaySdk",
+    name: "MAFPay",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
