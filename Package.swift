5f8c87aa4cc23938825985cedc394bd39605d9b3
Basheer Alhader <basheer@beamwallet.me>
2023-01-13 15:02:15 +0300

    adding Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "MAFPay",
+    name: "MAFPaySdk",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -18,9 +18,10 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
+      
+  	.binaryTarget(
             name: "MAFPay",
-            dependencies: []),
+            path: "MAFPay.xcframework")
  	.binaryTarget(
             name: "BinancePaySDK",
             path: "Frameworks/ExternalFrameworks/BinancePaySDK.xcframework"
@@ -32,9 +33,7 @@ let package = Package(
         .binaryTarget(
             name: "CardinalMobile",
             path: "Frameworks/ExternalFrameworks/CardinalMobile.xcframework"
-        ),
-        .testTarget(
-            name: "MAFPayTests",
-            dependencies: ["MAFPay"]),
+        )
+    
     ]
 )
