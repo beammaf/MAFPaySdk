b2be36588e93879603563315a7017c1b7bd12318
basheerhader <51642234+basheerhader@users.noreply.github.com>
2023-01-16 12:46:48 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,27 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "MAFPaySdk",
+
+    name: "MAFPaySPM",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "MAFPaySdk",
-            targets: ["MAFPaySdk"]),
+            name: "MAFPay",
+            targets: ["MAFPay", "BinancePaySDK", "CardinalMobile", "LeanSDK"])
     ],
-    dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire.git",
-                 branch: "5.6.2")
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "MAFPaySdk",
-            dependencies: ["Alamofire",
-                           "MAFPay",
-                           "BinancePaySDK",
-                           "CardinalMobile",
-                           "LeanSDK"]),
         .binaryTarget(name: "MAFPay", path: "MAFPay.xcframework"),
         .binaryTarget(name: "BinancePaySDK", path: "ExternalFrameworks/BinancePaySDK.xcframework"),
         .binaryTarget(name: "CardinalMobile", path: "ExternalFrameworks/CardinalMobile.xcframework"),
