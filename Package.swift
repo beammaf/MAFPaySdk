fff3f0e151927d21f37a07f3e1731d7543887a8d
Basheer Alhader <basheer@beamwallet.me>
2023-01-16 12:28:58 +0300

    adding Package.swift

--- Package.swift
+++ Package.swift
@@ -8,18 +8,26 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "MAFPay",
-            targets: ["MAFPay"]),
+            name: "MAFPaySdk",
+            targets: ["MAFPaySdk"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git",
+                 branch: "5.6.2")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(
-            name: "MAFPay",
-            path: "MAFPay.xcframework")
+        .target(
+            name: "MAFPaySdk",
+            dependencies: ["Alamofire",
+                           "MAFPay",
+                           "BinancePaySDK",
+                           "CardinalMobile",
+                           "LeanSDK"]),
+        .binaryTarget(name: "MAFPay", path: "MAFPay.xcframework"),
+        .binaryTarget(name: "BinancePaySDK", path: "ExternalFrameworks/BinancePaySDK.xcframework"),
+        .binaryTarget(name: "CardinalMobile", path: "ExternalFrameworks/CardinalMobile.xcframework"),
+        .binaryTarget(name: "LeanSDK", path: "ExternalFrameworks/LeanSDK.xcframework"),
     ]
 )
